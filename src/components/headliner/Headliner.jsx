ad7f3eaf40c7733dbf31e3b00bf730762013bebe
krutko77 <70036691+krutko77@users.noreply.github.com>
2021-10-04 22:14:51 +0300

    component Headliner

---
+++ src/components/headliner/Headliner.jsx
@@ -0,0 +1,19 @@
+import './Headliner.css';
+
+function Headliner() {
+  return (
+    <article className="headliner">
+      <img className="" src="" alt="photo" />
+      <div className="">
+         <h1 className="">Hate speech vs free speech</h1>
+         <div className="">
+         <div className=""></div>
+         <div className=""></div>
+         </div>
+         
+      </div>
+    </article>
+  );
+}
+
+export default Headliner;
