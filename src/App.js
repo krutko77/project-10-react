01e8745b64e40401ce58e6085cf3e8cde71144a2
krutko77 <70036691+krutko77@users.noreply.github.com>
2021-10-07 00:59:51 +0300

    headliner props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
 import Headliner from './components/headliner/Headliner';
+import img1 from './assets/images/img-1.jpg';
+import img2 from './assets/images/img-2.jpg';
+import img3 from './assets/images/img-3.jpg';
+import img4 from './assets/images/img-4.jpg';
+import img5 from './assets/images/img-5.jpg';
+import img6 from './assets/images/img-6.jpg';
 
 function App() {
   return (
     <div className="App">
-      <Headliner/>     
+      <Headliner title="Hate speech vs free speech" thumbnail={img1} label="BBC NEWS" likes="10.8k"
+      dislikes="1.4k" time="2:00 pm"/> 
+      <Headliner title="Ontario Liberal leadership"thumbnail={img2} label="BBC NEWS" likes="10.8k"
+      dislikes="3.4k" time="2:00 pm"/> 
+      <Headliner title="Victim of Scarborough"thumbnail={img3} label="BBC NEWS" likes="20.8k"
+      dislikes="1.1k" time="2:00 pm"/> 
+      <Headliner title="Mother who left her baby"thumbnail={img4} label="ABC NEWS" likes="0"
+      dislikes="20k" time="2:00 pm"/> 
+      <Headliner title="Mother of Stefanie Rengel"thumbnail={img5} label="CW6 NEWS" likes="10.8k"
+      dislikes="1.4k" time="2:00 pm"/> 
+      <Headliner title="World Cup: Popular items"thumbnail={img6} label="CNN NEWS" likes="10.8k"
+      dislikes="1.4k" time="2:00 pm"/>     
     </div>
   );
 }
--- src/components/headliner/Headliner.jsx
+++ src/components/headliner/Headliner.jsx
@@ -1,31 +1,30 @@
 import React from 'react';
 import './Headliner.css';
-import thumbnail from './../../assets/images/img-1.jpg';
 import likeIcon from './../../assets/images/like.svg';
 import disLikeIcon from './../../assets/images/dislike.svg';
 import clockIcon from './../../assets/images/clock.svg';
 
-function Headliner() {
+function Headliner(props) {
    return (
       <article className="headliner">
-         <img className="thumbnail-img" src={thumbnail} alt="photo" />
+         <img className="thumbnail-img" src={props.thumbnail} alt="photo" />
          <div className="info">
-            <h1 className="title">Hate speech vs free speech</h1>
+            <h1 className="title">{props.title}</h1>
             <div className="rating">
                <div className="like">
                   <img className="like-icon" src={likeIcon} alt="like" />
-                  <span className="text">10.9k</span>
+                  <span className="text">{props.likes}</span>
                </div>
                <div className="dislike">
                   <img className="dislike-icon" src={disLikeIcon} alt="dislike" />
-                  <span className="text">1.4k</span>
+                  <span className="text">{props.dislikes}</span>
                </div>
             </div>
             <div className="wrapper">
-               <span className="label">BBC NEWS</span>
+               <span className="label">{props.label}</span>
                <div className="time">
                   <img className="time-icon" src={clockIcon} alt="clock-icon" />
-                  <span className="time-text text">2:00 pm</span>
+                  <span className="time-text text">{props.time}</span>
                </div>
             </div>
          </div>
