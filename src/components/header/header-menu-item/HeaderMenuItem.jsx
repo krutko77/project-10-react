1741a3c7d66dd0346a606ba453bce6f773d05fc0
krutko77 <70036691+krutko77@users.noreply.github.com>
2022-04-13 23:08:29 +0300

    add svg

--- src/components/header/header-menu-item/HeaderMenuItem.jsx
+++ src/components/header/header-menu-item/HeaderMenuItem.jsx
@@ -7,8 +7,12 @@ function HeaderMenuItem(props) {
         <img className={s.menuImg} src={props.img} alt="img" />
         <span className={s.menuText}>{props.itemLabel}</span>
         <div className={s.buttons}>
-           <button className={s.buttonUp}></button>
-           <button className={s.buttonDown}></button>
+           <button className={s.buttonUp}><svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M8.825 6L5 2.28841L1.175 6L0 4.85175L5 0L10 4.85175L8.825 6Z" fill="black"/>
+</svg></button>
+           <button className={s.buttonDown}><svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1.175 0L5 3.71159L8.825 0L10 1.14825L5 6L0 1.14825L1.175 0Z" fill="black"/>
+</svg></button>
         </div>
      </div>
    );
